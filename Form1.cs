ed7ecf81aaf206a3a0ad82fc6d4ccacf7ec748d0
Gurb_bx51@itstep.edu.az <Gurb_bx51@itstep.edu.az>
2021-11-14 12:52:31 +0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using CountriesLinq.SeedData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountriesLinq
+{
+    public partial class Form1 : Form
+    {
+        CountriesDBEntities countriesDB;
+        public Form1()
+        {
+            InitializeComponent();
+            countriesDB = new CountriesDBEntities();
+            comboBox1.Items.Add("Select all continents");
+            comboBox1.Items.Add("Select all countries");
+            comboBox1.Items.Add("Select countries of");
+            comboBox1.Items.Add("Select cities of");
+            comboBox1.Items.Add("Select all capitals");
+            comboBox1.Items.Add("Top 5 largest countries");
+            comboBox1.Items.Add("Top 5 populated countries");
+            comboBox1.Items.Add("Top 5 populated cities");
+            comboBox1.Items.Add("Top 3 largest continents");
+            comboBox1.Items.Add("Top 3 populated continents");
+            foreach (var item in countriesDB.Continents)
+            {
+                comboBoxCont.Items.Add (item.Name);
+            }
+            foreach (var item in countriesDB.Countries)
+            {
+                comboBoxCountries.Items.Add(item.Name);
+            }
+            comboBoxCont.Visible=false;
+            comboBoxCountries.Visible=false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (countriesDB.Database.CreateIfNotExists())
+            {
+                countriesDB.SaveChanges();
+                MessageBox.Show("DB is created!");
+            }
+            else
+            {
+                MessageBox.Show("DB was already created!");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataSeeder seeder = new DataSeeder(countriesDB.Database.Connection.ConnectionString);
+            seeder.SeedData();
+            countriesDB.SaveChanges();
+            MessageBox.Show("Data is seeded!");
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            
+            switch ((sender as ComboBox).SelectedIndex)
+            {
+                case 0:
+                    dataGridView1.DataSource = countriesDB.Continents.ToList();  
+                    break;
+                case 1:
+                    dataGridView1.DataSource=countriesDB.Countries.ToList();
+                    break;
+                case 2:
+                    comboBoxCont.Visible = true;
+                    return;
+                case 3:
+                    comboBoxCountries.Visible = true;
+                    return;
+                case 4:
+                    dataGridView1.DataSource = countriesDB.Cities.Where(c => c.IsCapital == true).ToList();
+                    break;
+                case 5:
+                    dataGridView1.DataSource = (from count in countriesDB.Countries
+                                                orderby count.Area descending
+                                                select new
+                                                {
+                                                    name = count.Name,
+                                                    Area = count.Area
+                                                }
+
+                                                ).Take(5).ToList();
+                    break;
+                case 6:
+                    dataGridView1.DataSource = (from count in countriesDB.Countries
+                                                orderby count.Cities.Sum(x=>x.Population) descending
+                                                select new
+                                                {
+                                                    name = count.Name,
+                                                    Population = count.Cities.Sum(x => x.Population)
+                                                }
+
+                                                ).Take(5).ToList();
+                    break;
+                case 7:
+                    dataGridView1.DataSource = (from city in countriesDB.Cities
+                                                orderby city.Population descending
+                                                select new
+                                                {
+                                                    name = city.Name,
+                                                    Population = city.Population
+                                                }
+
+                                                ).Take(5).ToList();
+                    break;
+                case 8:
+                    dataGridView1.DataSource = (from cont in countriesDB.Continents
+                                                orderby cont.Countries.Sum(x => x.Area) descending
+                                                select new
+                                                {
+                                                    Name = cont.Name,
+                                                    Area = cont.Countries.Sum(x => x.Area)
+                                                }
+
+                                               ).Take(3).ToList();
+                    break;
+                case 9:
+                    dataGridView1.DataSource = (from cont in countriesDB.Continents
+                                                orderby cont.Countries.Sum(x => x.Cities.Sum(p=>p.Population)) descending
+                                                select new
+                                                {
+                                                    Name = cont.Name,
+                                                    Population = cont.Countries.Sum(x => x.Cities.Sum(p => p.Population))
+                                                }
+
+                                               ).Take(3).ToList();
+                    break;
+                default:
+                    break;
+            }
+            comboBoxCont.Visible=false;
+            comboBoxCountries.Visible=false;
+        }
+        private void comboBoxCont_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int ContId = (sender as ComboBox).SelectedIndex+1;
+            dataGridView1.DataSource=countriesDB.Countries.Where(x=>x.ContinentId==ContId).ToList();
+        }
+
+        private void comboBoxCountries_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int CountId = (sender as ComboBox).SelectedIndex+1;
+            dataGridView1.DataSource = countriesDB.Cities.Where(x => x.CountryId == CountId).ToList();
+        }
+    }
+}
---
+++ SeedData/DataSeeder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountriesLinq.SeedData
+{
+    class DataSeeder
+    {
+        public string ConnectionString { get; set; }
+        public DataSeeder(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+        public void SeedData()
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+
+                string str1 = @"INSERT INTO Continents (Name) VALUES ('Africa');
+INSERT INTO Continents (Name) VALUES ('America');
+INSERT INTO Continents (Name) VALUES ('Eurasia');
+INSERT INTO Continents (Name) VALUES ('Australia');
+INSERT INTO Continents (Name) VALUES ('Antarctida');";
+                string str2 = @"INSERT INTO Countries (Name,ContinentId,Area)VALUES ('France',3,2000000);
+INSERT INTO Countries (Name,ContinentId,Area)VALUES ('Greece',3,200000);
+INSERT INTO Countries (Name,ContinentId,Area)VALUES ('England',3,1500000);
+INSERT INTO Countries (Name,ContinentId,Area)VALUES ('Azerbaijan',3,90000);
+INSERT INTO Countries (Name,ContinentId,Area)VALUES ('Brazil',2,2480000);
+INSERT INTO Countries (Name,ContinentId,Area)VALUES ('Australia',4,7000000);
+INSERT INTO Countries (Name,ContinentId,Area)VALUES ('C1',5,5000000);
+INSERT INTO Countries (Name,ContinentId,Area)VALUES ('OAE',1,1200000);
+INSERT INTO Countries (Name,ContinentId,Area)VALUES ('Kenia',1,190000);
+INSERT INTO Countries (Name,ContinentId,Area)VALUES ('Canada',2,1200000);";
+                string str3 = @"INSERT INTO Cities (Name,CountryId,Population,IsCapital)VALUES ('Baku',4,3000000,1);
+INSERT INTO Cities (Name,CountryId,Population,IsCapital)VALUES ('Paris',1,10000000,1);
+INSERT INTO Cities (Name,CountryId,Population,IsCapital)VALUES ('London',3,15000000,1);
+INSERT INTO Cities (Name,CountryId,Population,IsCapital)VALUES ('Brazilia',5,19000000,1);
+INSERT INTO Cities (Name,CountryId,Population,IsCapital)VALUES ('Lankaran',4,500000,0);
+INSERT INTO Cities (Name,CountryId,Population,IsCapital)VALUES ('Strasbourg',1,2000000,0);
+INSERT INTO Cities (Name,CountryId,Population,IsCapital)VALUES ('Rio',5,11000000,0);";
+                SqlCommand cmd = new SqlCommand(str1, con);
+                SqlCommand cmd2 = new SqlCommand(str2, con);
+                SqlCommand cmd3 = new SqlCommand(str3, con);
+                cmd.ExecuteNonQuery();
+                cmd2.ExecuteNonQuery();
+                cmd3.ExecuteNonQuery();
+            }
+        }
+    }
+}
